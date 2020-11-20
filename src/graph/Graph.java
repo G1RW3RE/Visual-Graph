a0404d022564bfb6a640d18cb92c775b4a22089b
gongzhenyu <gongzhenyu@kyee.com.cn>
2020-11-20 16:24:14 +0800

    First commit

---
+++ src/graph/Graph.java
@@ -0,0 +1,23 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ggloria
+ */
+public class Graph {
+
+    public List<Vertex> vertexes;
+
+    public Graph() {
+        vertexes = new ArrayList<>();
+    }
+
+    public void addVertex(Vertex vertex) {
+        if(vertex != null) {
+            vertexes.add(vertex);
+        }
+    }
+
+}
